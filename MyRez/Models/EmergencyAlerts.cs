4d67b458415853a0e28f9192bdc28f4129ae8049
Sudeep Neupane <sudmic@outlook.com>
2019-11-29 15:06:25 -0600

    EmergencyAlerts.cs

---
+++ MyRez/Models/EmergencyAlerts.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyRez.Models
+{
+    public class Users
+    {
+        public int EmergencyID { get; set; }
+        public String EmergencyCategory { get; set; }
+        public String EmergencyMessage { get; set; }
+        public int AdmID { get; set; }
+    }
+}
